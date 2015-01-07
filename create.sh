183481802413aad54a0ca56b97e4eacffb865328
Axel Etcheverry <euskadi31@gmail.com>
2015-01-07 10:29:26 +0100

    chore(script): WIP

--- create.sh
+++ create.sh
@@ -5,8 +5,8 @@
 
 GENTOO_FUNC=${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}/isolated-functions.sh
 DATA_DIR=$(pwd)/data
-IMAGE_NAME="gentoo-base"
 LOGGER=$(pwd)/create.log
+BUILD_DIR=$(pwd)/build
 
 if [ ! -f $GENTOO_FUNC ]; then
     source eapi.sh
@@ -31,6 +31,9 @@ if [ ! -d $DATA_DIR ]; then
     mkdir -p $DATA_DIR
 fi
 
+if [ ! -d $BUILD_DIR ]; then
+    mkdir -p $BUILD_DIR
+fi
 
 if [[ -z $STAGE3 ]]
 then
@@ -43,6 +46,10 @@ einfo "Release: ${STAGE3:0:4}-${STAGE3:4:2}-${STAGE3:6}"
 
 STAGE3_FILE="$DATA_DIR/stage3-amd64-$STAGE3.tar.bz2"
 
+IMAGE_NAME="gentoo-temp:stage3-amd64-$STAGE3"
+CONTAINER_NAME="gentoo-temp-stage3-amd64-$STAGE3"
+CONTAINER_FILE="$BUILD_DIR/gentoo-base.tar.xz"
+
 SRC="http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3/stage3-amd64-$STAGE3.tar.bz2"
 
 if [ ! -f "$STAGE3_FILE" ]; then
@@ -52,11 +59,56 @@ if [ ! -f "$STAGE3_FILE" ]; then
 fi
 
 ebegin "Import stage3 in docker"
-bzcat "$STAGE3_FILE" | docker import - "$IMAGE_NAME" 2>> $LOGGER
+bzcat "$STAGE3_FILE" | docker import - "$IMAGE_NAME" > /dev/null 2>> $LOGGER
 eend $?
 
 if [ $? ]; then
     exit 1
 fi
 
+ebegin "Remove old Gentoo container"
+docker rm -f "$CONTAINER_NAME" > /dev/null 2>> $LOGGER || true
+eend $?
+
+ebegin "Configure Gentoo"
+docker run -t -v /usr/portage:/usr/portage:ro --name "$CONTAINER_NAME" "$IMAGE_NAME" bash -exc $'
+    export MAKEOPTS="-j$(nproc)"
+    pythonTarget="$(emerge --info | sed -n \'s/.*PYTHON_TARGETS="\\([^"]*\\)".*/\\1/p\')"
+    pythonTarget="${pythonTarget##* }"
+    echo \'PYTHON_TARGETS="\'$pythonTarget\'"\' >> /etc/portage/make.conf
+    echo \'PYTHON_SINGLE_TARGET="\'$pythonTarget\'"\' >> /etc/portage/make.conf
+    emerge --newuse --deep --with-bdeps=y @system @world
+    emerge -C editor ssh man man-pages openrc e2fsprogs texinfo service-manager
+    emerge --depclean
+' >> $LOGGER
+eend $?
+
+ebegin "Export container"
+docker export "$CONTAINER_NAME" | xz -9 > "$CONTAINER_FILE" >> $LOGGER
+eend $?
+
+ebegin "Remove container"
+docker rm "$CONTAINER_NAME" >> $LOGGER
+eend $?
+
+ebegin "Remove image"
+docker rmi "$IMAGE_NAME" >> $LOGGER
+eend $?
+
+echo 'FROM scratch' > $BUILD_DIR/Dockerfile
+echo "ADD $CONTAINER_FILE /" >> $BUILD_DIR/Dockerfile
+echo 'CMD ["/bin/bash"]' >> $BUILD_DIR/Dockerfile
+
+ebegin "Fetch username"
+DOCKER_USER=$(docker info | awk '/^Username:/ { print $2 }')
+if [ -z "$DOCKER_USER" ]; then
+    eend 1
+    exit $?
+else
+    eend 0
+fi
+
+ebegin "Build container"
+docker build -t "$DOCKER_USER/gentoo-base" . >> $LOGGER
+eend $?
 
